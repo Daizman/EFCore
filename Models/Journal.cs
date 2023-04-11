8f5fedba6ba4fbb27ab947920f6ff11e1ae3e02e
Daziman <andihell@yandex.ru>
2023-04-11 20:13:40 +0500

    set journal model

--- Models/Journal.cs
+++ Models/Journal.cs
@@ -2,6 +2,33 @@ namespace EFCore.Models
 {
     public class Journal : Book
     {
-        
+        private int _edition;
+
+        public Journal(
+            Guid id,
+            string title,
+            DateOnly publishDate,
+            Guid publisherId,
+            List<Genre> genres,
+            List<Author> authors,
+            int edition
+        ) : base(id, title, publishDate, publisherId, genres, authors)
+        {
+            _edition = edition;
+        }
+
+        public int Edition 
+        {
+            get => _edition;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("Edition can't be below zero");
+                }
+
+                _edition = value;
+            }
+        }
     }
 }
