90ea565b223737adedec04ac11940de4ebdb8b49
Daziman <andihell@yandex.ru>
2023-04-15 21:18:55 +0500

    add genres seed data

--- Configuration/DbEntitiesConfiguration/GenreConfiguration.cs
+++ Configuration/DbEntitiesConfiguration/GenreConfiguration.cs
@@ -10,6 +10,17 @@ namespace EFCore.Configuration.DbEntitiesConfiguration
         {
             builder.HasIndex(genre => genre.Id).IsUnique();
             builder.Property(genre => genre.Name).HasMaxLength(128);
+            builder.HasData(
+                new Genre(1, "Ужасы"),
+                new Genre(2, "Комедия"),
+                new Genre(3, "Детектив"),
+                new Genre(4, "Триллер"),
+                new Genre(5, "Спорт"),
+                new Genre(6, "Драма"),
+                new Genre(7, "Романтика"),
+                new Genre(8, "Фэнтези"),
+                new Genre(9, "Приключения")
+            );
         }
     }
 }
---
+++ Migrations/20230415161805_initGenres.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class initGenres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Ужасы" },
+                    { 2, "Комедия" },
+                    { 3, "Детектив" },
+                    { 4, "Триллер" },
+                    { 5, "Спорт" },
+                    { 6, "Драма" },
+                    { 7, "Романтика" },
+                    { 8, "Фэнтези" },
+                    { 9, "Приключения" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 9);
+        }
+    }
+}
